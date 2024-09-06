64de7b89968f79d336fed8c8771009fda9a341af
Lucas Marques <luandrealbuquerque@gmail.com>
2024-09-05 22:16:30 -0300

    chore: Update project configurations and dependencies

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,19 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Text } from "./src/components/Text";
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-700": require("./src/assets/fonts/GeneralSans-Bold.otf"),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start work on your app!</Text>
