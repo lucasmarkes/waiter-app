0586c29d062fc7893adc99b7f55a52c54477971c
Lucas Marques <luandrealbuquerque@gmail.com>
2024-09-25 18:27:15 -0300

    feat: Add socket.io dependency for real-time updates

--- api/src/app/useCases/orders/createOrder.ts
+++ api/src/app/useCases/orders/createOrder.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
+import { io } from "../../..";
 import { Order } from "../../models/Order";
 
 export async function createOrder(req: Request, res: Response) {
   try {
     const { products, table } = req.body;
 
-    const order = await Order.create({ products, table });
+    const order = await Order.create({ table, products });
 
+    const orderDetails = await order.populate('products.product')
+
+    io.emit("orders@new", orderDetails);
     return res.status(201).json(order);
   } catch (error) {
     return res.sendStatus(500);
